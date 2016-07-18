a9fad938711b46a35a089ec5ca7da3230dc261de
melicarls <melissacarlson15@gmail.com>
2016-07-18 14:41:10 -0700

    sprint 3 step 1

--- models/album.js
+++ models/album.js
@@ -1,11 +1,12 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var AlbumSchema = new Schema({
   artistName: String,
   name: String,
   releaseDate: String,
-  genres: Array
+  genres: Array,
+  songs: [ Song.schema ]
 });
 
 var Album=mongoose.model('Album', AlbumSchema);
