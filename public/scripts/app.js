23cc0c381b5299fc20626f2892a829aa9d3cca37
melicarls <melissacarlson15@gmail.com>
2016-07-18 13:07:35 -0700

    sprint 1 steps 4 and 5

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,38 +5,9 @@
  *
  */
 
-
-/* hard-coded data! */
-var sampleAlbums = [];
-sampleAlbums.push({
-             artistName: 'Ladyhawke',
-             name: 'Ladyhawke',
-             releaseDate: '2008, November 18',
-             genres: [ 'new wave', 'indie rock', 'synth pop' ]
-           });
-sampleAlbums.push({
-             artistName: 'The Knife',
-             name: 'Silent Shout',
-             releaseDate: '2006, February 17',
-             genres: [ 'synth pop', 'electronica', 'experimental' ]
-           });
-sampleAlbums.push({
-             artistName: 'Juno Reactor',
-             name: 'Shango',
-             releaseDate: '2000, October 9',
-             genres: [ 'electronic', 'goa trance', 'tribal house' ]
-           });
-sampleAlbums.push({
-             artistName: 'Philip Wesley',
-             name: 'Dark Night of the Soul',
-             releaseDate: '2008, September 12',
-             genres: [ 'piano' ]
-           });
-/* end of hard-coded data */
-
 var template;
 var $albumsList;
-// var allAlbums=[];
+var allAlbums=[];
 
 
 
@@ -53,10 +24,6 @@ $(document).ready(function() {
     url: '/api/albums',
     success: onSuccess,
     error: onError
-  });
-
-  sampleAlbums.forEach(function(album) {
-    renderAlbum(album);
   });
 
 });
