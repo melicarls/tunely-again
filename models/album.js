03588adf7b63371ab6a8b9fc2ad1090ebe10588f
melicarls <melissacarlson15@gmail.com>
2016-07-18 16:12:26 -0700

    seeded song data

--- models/album.js
+++ models/album.js
@@ -1,11 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var Song = require('./song');
+
 var AlbumSchema = new Schema({
   artistName: String,
   name: String,
   releaseDate: String,
-  genres: Array,
+  genres: [ String ],
   songs: [ Song.schema ]
 });
 
